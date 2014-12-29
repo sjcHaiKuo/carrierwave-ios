ca04184aedf70e0218dacf5dc0810d390917b0fe
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2014-12-29 09:29:50 +0100

    Add tests for initialization with data
    [#85114636]

--- Unit Tests/CRVImageAssetSpec.m
+++ Unit Tests/CRVImageAssetSpec.m
@@ -59,6 +59,102 @@ describe(@"CRVImageAsset", ^{
 
     });
 
+    sharedExamplesFor(@"asset guessing the correct mime type", ^(NSDictionary *data) {
+
+        __block CRVImageAsset *asset = nil;
+
+        beforeEach(^{
+            NSString *fileName = data[@"name"], *fileExtension = data[@"extension"];
+            NSString *filePath = [[NSBundle bundleForClass:[self class]] pathForResource:fileName ofType:fileExtension];
+            NSData *fileData = [[NSData alloc] initWithContentsOfFile:filePath];
+            asset = [[CRVImageAsset alloc] initWithData:fileData fileName:nil mimeType:nil];
+        });
+
+        it(@"should have a data representation", ^{
+            expect(asset.data).toNot.beNil();
+        });
+
+        it(@"should not have a file name", ^{
+            expect(asset.fileName).to.beNil();
+        });
+
+        it(@"should have a correct mime type", ^{
+            NSString *expectedMimeType = data[@"mime"];
+            expect(asset.mimeType).to.equal(expectedMimeType);
+        });
+
+    });
+
+    context(@"when initialized with bmp data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"bmp",
+            @"mime": @"image/x-ms-bmp",
+        });
+
+    });
+
+    context(@"when initialized with gif data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"gif",
+            @"mime": @"image/gif",
+        });
+
+    });
+
+    context(@"when initialized with jpg data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"jpg",
+            @"mime": @"image/jpeg",
+        });
+
+    });
+
+    context(@"when initialized with png data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"png",
+            @"mime": @"image/png",
+        });
+
+    });
+
+    context(@"when initialized with psd data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"psd",
+            @"mime": @"image/psd",
+        });
+
+    });
+
+    context(@"when initialized with tiff data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"tiff",
+            @"mime": @"image/tiff",
+        });
+
+    });
+
+    context(@"when initialized with bmp data", ^{
+
+        itShouldBehaveLike(@"asset guessing the correct mime type", @{
+            @"name": @"pixel",
+            @"extension": @"webp",
+            @"mime": @"image/webp",
+        });
+
+    });
+
 });
 
 SpecEnd
