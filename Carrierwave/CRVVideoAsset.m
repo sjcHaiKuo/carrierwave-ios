304b2af97c60090d3c977fd945a0dd89569805dd
Wojceich Trzasko <wojciech.trzasko@gmail.com>
2015-01-15 15:14:49 +0100

    [#85362080][WIP] Guessing signature form stream.

--- Carrierwave/CRVVideoAsset.m
+++ Carrierwave/CRVVideoAsset.m
@@ -49,13 +49,56 @@
 
 #pragma mark - Mimetypes helpers
 
-CRVWorkInProgress("Temporary. Need to change for quering mimetype from data.");
+CRVWorkInProgress("Temporary. Still work in progress. Needs tests.");
 
-// Need to check how to query extension info from input stream, without affecting on stream itself.
+// Need to check how to query extension info from input stream, without affecting stream itself.
 // According to Apple documentatio: Once opened, a stream cannot be closed and reopened.
 
-- (NSString *)mimeTypeByGuessing {
-    return @"video/mp4";
+- (NSString *)mimeTypeByGuessingFromStream:(NSInputStream *)stream {
+    
+    // read bytes
+    uint8_t bytes[16] = {0};
+    
+    [stream open];
+    NSInteger readBytesCount = [stream read:bytes maxLength:sizeof(bytes)];
+    [stream close];
+    
+    // if error or empty
+    if (readBytesCount <= 0) {
+        return nil;
+    }
+    
+    // initialize signatures
+    const char mov1[8] = { 0x00, 0x00, 0x00, 0x14, 0x66, 0x74, 0x79, 0x70 };
+    const char mov2[8] = { 0x00, 0x00, 0x00, 0x00, 0x6D, 0x6F, 0x6F, 0x76 };
+    const char mov3[8] = { 0x00, 0x00, 0x00, 0x00, 0x71, 0x74, 0x20, 0x20 };
+    const char mov4[8] = { 0x00, 0x00, 0x00, 0x00, 0x66, 0x72, 0x65, 0x65 };
+    const char mov5[8] = { 0x00, 0x00, 0x00, 0x00, 0x6D, 0x64, 0x61, 0x74 };
+    const char mov6[8] = { 0x00, 0x00, 0x00, 0x00, 0x77, 0x69, 0x64, 0x65 };
+    const char mov7[8] = { 0x00, 0x00, 0x00, 0x00, 0x70, 0x6E, 0x6F, 0x74 };
+    const char mov8[8] = { 0x00, 0x00, 0x00, 0x00, 0x73, 0x6B, 0x69, 0x70 };
+    
+    // full mp4 file signature:
+    // 00 00 00 nn 66 74 79 70 33 67 70 35
+    unsigned int mp4Offset = 4;
+    const char mp4[9] = { 0x66, 0x74, 0x79, 0x70, 0x33, 0x67, 0x70, 0x35 };
+    
+    // try to guess by signatures
+    if (!memcmp(bytes, mov1, 8)
+        || !memcmp(bytes, mov2, 8)
+        || !memcmp(bytes, mov3, 8)
+        || !memcmp(bytes, mov4, 8)
+        || !memcmp(bytes, mov5, 8)
+        || !memcmp(bytes, mov6, 8)
+        || !memcmp(bytes, mov7, 8)
+        || !memcmp(bytes, mov8, 8)) {
+        return @"video/quicktime";
+    } else if (!memcmp(bytes + mp4Offset, mp4, 13)) {
+        return @"video/mp4";
+    }
+    
+    // default
+    return nil;
 }
 
 #pragma mark - Property accessors
@@ -67,7 +110,7 @@ CRVWorkInProgress("Temporary. Need to change for quering mimetype from data.");
 
 - (NSString *)mimeType {
     if (_mimeType != nil) return _mimeType;
-    return _mimeType = [self mimeTypeByGuessing];
+    return _mimeType = [self mimeTypeByGuessingFromStream:self.dataStream];
 }
 
 @end
