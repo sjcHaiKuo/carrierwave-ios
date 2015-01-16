7657fdd25ffb15503d7681037a720670841db5b0
Wojceich Trzasko <wojciech.trzasko@gmail.com>
2015-01-16 10:32:28 +0100

    [#85362080] Additional signatures for mp4 files.

--- Carrierwave/CRVVideoAsset.m
+++ Carrierwave/CRVVideoAsset.m
@@ -57,7 +57,7 @@ CRVWorkInProgress("Temporary. Still work in progress. Needs tests.");
 - (NSString *)mimeTypeByGuessingFromStream:(NSInputStream *)stream {
     
     // read bytes
-    uint8_t bytes[16] = {0};
+    uint8_t bytes[32] = {0};
     
     [stream open];
     NSInteger readBytesCount = [stream read:bytes maxLength:sizeof(bytes)];
@@ -70,15 +70,22 @@ CRVWorkInProgress("Temporary. Still work in progress. Needs tests.");
     
     unsigned int signatureOffset = 4;
     
-    // initialize signatures
-    // based on http://www.garykessler.net/library/file_sigs.html
+    // signatures based on http://www.garykessler.net/library/file_sigs.html
+    // mov - ftypqt
     uint8_t mov[6] = { 0x66, 0x74, 0x79, 0x70, 0x71, 0x74 };
-    uint8_t mp4[9] = { 0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32 };
+    // mp4 - ftypisom
+    uint8_t mp4_isom[8] = { 0x66, 0x74, 0x79, 0x70, 0x69, 0x73, 0x6F, 0x6D};
+    // mp4 - ftyp3gp5
+    uint8_t mp4_3gp[8] = { 0x66, 0x74, 0x79, 0x70, 0x33, 0x67, 0x70, 0x35 };
+    // m4v - ftypmp42
+    uint8_t mp4_mp42[9] = { 0x66, 0x74, 0x79, 0x70, 0x6D, 0x70, 0x34, 0x32 };
     
     // try to guess by signatures
-    if (!memcmp(bytes + signatureOffset, mov, 6)) {
+    if (!memcmp(bytes + signatureOffset, mov, sizeof(mov))) {
         return @"video/quicktime";
-    } else if (!memcmp(bytes + signatureOffset, mp4, 8)) {
+    } else if (!memcmp(bytes + signatureOffset, mp4_isom, sizeof(mp4_isom))
+               || !memcmp(bytes + signatureOffset, mp4_3gp, sizeof(mp4_3gp))
+               || !memcmp(bytes + signatureOffset, mp4_mp42, sizeof(mp4_mp42))) {
         return @"video/mp4";
     }
     
