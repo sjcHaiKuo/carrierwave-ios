1e73f0f0fa3cde6b1be2b82cb6bb9f5c3da93952
Wojceich Trzasko <wojciech.trzasko@gmail.com>
2015-02-02 15:08:19 +0100

    Skeleton for CVRSaveAssetTask tests. [#86510208]

---
+++ Unit Tests/CRVSaveAssetTaskSpec.m
@@ -0,0 +1,53 @@
+//
+//  CRVSaveAssetTaskSpec.m
+//  Carrierwave
+//
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+SpecBegin(CRVSaveAssetTaskSpec)
+
+describe(@"CRVSaveAssetTaskSpec", ^{
+    
+    describe(@"using valid asset", ^{
+        
+        context(@"when output buffer has empty space", ^{
+            
+            pending(@"should save asset data to output", ^{
+                
+            });
+            
+            pending(@"should return path to cache file", ^{
+                
+            });
+            
+            pending(@"should return path to library file", ^{
+                
+            });
+            
+            pending(@"should return path to temporary file", ^{
+                
+            });
+            
+        });
+        
+        context(@"when output buffer has no empty space", ^{
+            
+            pending(@"should return error", ^{
+                
+            });
+        });
+        
+    });
+    
+    describe(@"when use invalid file path for asset", ^{
+        
+        pending(@"should return error", ^{
+            
+        });
+        
+    });
+    
+});
+
+SpecEnd
