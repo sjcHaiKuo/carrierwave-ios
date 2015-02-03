b27fec70e5d2cb83c8ebfa961afbf98be4cadae2
Paweł Białecki <pawel.bialecki@netguru.pl>
2015-02-03 11:21:35 +0100

    Added tests for CRVAnchorPoint [#87204720]

--- Unit Tests/CRVAnchorPointSpec.m
+++ Unit Tests/CRVAnchorPointSpec.m
@@ -10,7 +10,41 @@ SpecBegin(CRVAnchorPointSpec)
 
 describe(@"CRVAnchorPointSpec", ^{
     
-//    __block CRVAnchorPoint *sut = nil;
+    __block CRVAnchorPoint *anchorPoint = nil;
+    
+    context(@"when newly created, ", ^{
+        
+        
+        
+        describe(@"with center location, ", ^{
+            
+            beforeEach(^{
+                anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationCenter];
+            });
+            
+            it(@"it should be initialized with center location in property.", ^{
+                expect(anchorPoint.location).to.equal(CRVAnchorPointLocationCenter);
+                expect(anchorPoint.location).toNot.equal(CRVAnchorPointLocationMiddleRight);
+            });
+        });
+        
+        
+        
+        describe(@"with some other location than center, ", ^{
+            
+            CRVAnchorPointLocation otherLocation = CRVAnchorPointLocationBottomRight;
+            
+            beforeEach(^{
+                anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:otherLocation];
+            });
+            
+            it(@"it should be initialized with some other location than center.", ^{
+                expect(anchorPoint.location).toNot.equal(CRVAnchorPointLocationCenter);
+                expect(anchorPoint.location).to.equal(otherLocation);
+            });
+        });
+        
+    });
     
 });
 
