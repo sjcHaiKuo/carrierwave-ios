c7aad8ae4707676a13a365c202fda29c544ea57b
Paweł Białecki <pawel.bialecki@netguru.pl>
2015-02-09 15:10:07 +0100

    Improved tests for CRVAnchorPoint
    [#87204720]

--- Unit Tests/CRVAnchorPointSpec.m
+++ Unit Tests/CRVAnchorPointSpec.m
@@ -12,133 +12,103 @@ describe(@"CRVAnchorPointSpec", ^{
     
     __block CRVAnchorPoint *anchorPoint = nil;
     
+    beforeEach(^{
+        anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationCenter];
+    });
+    
+    afterEach(^{
+        anchorPoint = nil;
+    });
+    
     context(@"when newly created", ^{
-        
-        beforeEach(^{
-            anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationCenter];
-        });
         
         it(@"it should have 9 CRVAnchorPointLocation enum elements", ^{
             expect(CRVAnchorPointLocationPointsCount).to.equal(9);
         });
+    });
         
-        context(@"with center location", ^{
+    context(@"with center location", ^{
             
-            CRVAnchorPointLocation centerLocation = CRVAnchorPointLocationCenter;
+        CRVAnchorPointLocation centerLocation = CRVAnchorPointLocationCenter;
             
-            beforeEach(^{
-                anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:centerLocation];
-            });
-            
-            it(@"it should be initialized with center location", ^{
-                expect(anchorPoint.location).to.equal(centerLocation);
-            });
-            
-            it(@"it should be not initialized with some other location than center", ^{
-                expect(anchorPoint.location).toNot.equal(CRVAnchorPointLocationMiddleRight);
-            });
-            
-            it(@"it should give a proper name", ^{
-                expect([anchorPoint locationName]).to.equal(@"Center");
-            });
-            
-            it(@"it should have a proper adjusts and ratios", ^{
-                expect(anchorPoint.adjustsX).to.equal(0);
-                expect(anchorPoint.adjustsY).to.equal(0);
-                expect(anchorPoint.adjustsH).to.equal(0);
-                expect(anchorPoint.adjustsW).to.equal(0);
-                expect(anchorPoint.ratioX1).to.equal(0);
-                expect(anchorPoint.ratioX2).to.equal(0);
-                expect(anchorPoint.ratioY1).to.equal(0);
-                expect(anchorPoint.ratioY2).to.equal(0);
-                expect(anchorPoint.ratioW).to.equal(0);
-                expect(anchorPoint.ratioH).to.equal(0);
-            });
-            
-            describe(@"using reference point", ^{
-                
-                CGPoint expectedPoint = CGPointMake(25.f, 50.f);
-                
-                before(^{
-                    CGSize size = CGSizeMake(50.f, 100.f);
-                    [anchorPoint setReferencePointWithSize:size];
-                });
-                
-                it(@"it should have a proper value", ^{
-                    expect(anchorPoint.referencePoint).to.equal(expectedPoint);
-                });
-                
-            });
+        it(@"should be initialized with center location", ^{
+            expect(anchorPoint.location).to.equal(centerLocation);
         });
-        
-        context(@"with some other location than center", ^{
             
-            CRVAnchorPointLocation otherLocation = CRVAnchorPointLocationBottomRight;
-            
-            beforeEach(^{
-                anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:otherLocation];
-            });
-            
-            it(@"it should be initialized with some other location than center", ^{
-                expect(anchorPoint.location).to.equal(otherLocation);
-            });
-            
-            it(@"it should be not initialized with center location", ^{
-                expect(anchorPoint.location).toNot.equal(CRVAnchorPointLocationCenter);
-            });
-            
-            it(@"it should give a proper name", ^{
-                expect([anchorPoint locationName]).to.equal(@"Bottom Right");
-            });
-            
-            it(@"it should have a proper adjusts and ratios", ^{
-                expect(anchorPoint.adjustsX).to.equal(0);
-                expect(anchorPoint.adjustsY).to.equal(0);
-                expect(anchorPoint.adjustsH).to.equal(1);
-                expect(anchorPoint.adjustsW).to.equal(-1);
-                expect(anchorPoint.ratioX1).to.equal(0);
-                expect(anchorPoint.ratioX2).to.equal(0);
-                expect(anchorPoint.ratioY1).to.equal(0);
-                expect(anchorPoint.ratioY2).to.equal(0);
-                expect(anchorPoint.ratioW).to.equal(0);
-                expect(anchorPoint.ratioH).to.equal(1);
-            });
-            
-            describe(@"using reference point", ^{
-                
-                CGPoint expectedPoint = CGPointMake(30.f, 140.f);
-                
-                before(^{
-                    CGSize size = CGSizeMake(30.f, 140.f);
-                    [anchorPoint setReferencePointWithSize:size];
-                });
-                
-                it(@"it should have a proper value", ^{
-                    expect(anchorPoint.referencePoint).to.equal(expectedPoint);
-                });
-                
-            });
-            
+        it(@"should be not initialized with some other location than center", ^{
+            expect(anchorPoint.location).toNot.equal(CRVAnchorPointLocationMiddleRight);
         });
-        
-        describe(@"distanceFromReferencePointToPoint", ^{
             
-            __block CGPoint point;
-            __block CGFloat distance;
-            
-            before(^{
-                point = CGPointMake(20.f, 30.f);
-                distance = [anchorPoint distanceFromReferencePointToPoint:point];
-            });
-            
-            it(@"should give a proper value", ^{
-                expect(distance).to.beCloseToWithin(36.f, 0.1f); // exact expected value: 36.05551275...
-            });
-            
+        it(@"should give a proper name", ^{
+            expect([anchorPoint locationName]).to.equal(@"Center");
         });
-        
+            
+        it(@"should have a proper adjusts and ratios", ^{
+            CRVAnchorPoint *centerAnchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationCenter];
+            CRVAnchorPoint *bottomRightAnchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationBottomRight];
+            
+            expect(anchorPoint).to.crv_beEqualToAnchorPoint(centerAnchorPoint);
+            expect(anchorPoint).toNot.crv_beEqualToAnchorPoint(bottomRightAnchorPoint);
+        });
     });
     
+    context(@"with some other location than center", ^{
+            
+        CRVAnchorPointLocation otherLocation = CRVAnchorPointLocationTopLeft;
+            
+        beforeEach(^{
+            anchorPoint = [[CRVAnchorPoint alloc] initWithLocation:otherLocation];
+        });
+            
+        it(@"should be initialized with some other location than center", ^{
+           expect(anchorPoint.location).to.equal(otherLocation);
+        });
+            
+        it(@"should be not initialized with center location", ^{
+            expect(anchorPoint.location).toNot.equal(CRVAnchorPointLocationCenter);
+        });
+            
+        it(@"should give a proper name", ^{
+            expect([anchorPoint locationName]).to.equal(@"Top Left");
+        });
+            
+        it(@"should have a proper adjusts and ratios", ^{
+            CRVAnchorPoint *topLeftAnchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationTopLeft];
+            CRVAnchorPoint *bottomMiddleAnchorPoint = [[CRVAnchorPoint alloc] initWithLocation:CRVAnchorPointLocationBottomMiddle];
+                
+            expect(anchorPoint).to.crv_beEqualToAnchorPoint(topLeftAnchorPoint);
+            expect(anchorPoint).toNot.crv_beEqualToAnchorPoint(bottomMiddleAnchorPoint);
+        });
+    });
+
+    describe(@"distance between reference point and touch point", ^{
+        
+        CGPoint expectedPoint = CGPointMake(25.f, 50.f);
+        
+        beforeEach(^{
+            CGSize size = CGSizeMake(50.f, 100.f);
+            [anchorPoint setReferencePointWithSize:size];
+        });
+        
+        it(@"should be calculated properly", ^{
+            expect(anchorPoint.referencePoint).to.equal(expectedPoint);
+        });
+    });
+    
+    describe(@"calculating distance from reference point to point", ^{
+            
+        __block CGPoint point;
+        __block CGFloat distance;
+            
+        beforeEach(^{
+            point = CGPointMake(20.f, 30.f);
+            distance = [anchorPoint distanceFromReferencePointToPoint:point];
+        });
+            
+        it(@"should be calculated properly", ^{
+            expect(distance).to.beCloseToWithin(36.f, 0.1f); // exact expected value: 36.05551275...
+        });
+    });
 });
 
 SpecEnd
