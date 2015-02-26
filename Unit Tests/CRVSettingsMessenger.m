6b18346680c3dbc9d6c90df4b390a811b8f614a1
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-02-26 12:15:19 +0100

    SettingsMessenger spec [#89151080]

---
+++ Unit Tests/CRVSettingsMessenger.m
@@ -0,0 +1,55 @@
+//
+//  CRVSettingsMessenger.m
+//  Carrierwave
+//
+//  Created by Patryk Kaczmarek on 26.02.2015.
+//  Copyright (c) 2015 Netguru Sp. z o.o. All rights reserved.
+//
+
+SpecBegin(CRVSettingsMessengerSpec)
+
+fdescribe(@"CRVSettingsMessenger", ^{
+    
+    __block CRVSettingsMessenger *sut;
+    
+    context(@"when initialized", ^{
+        
+        beforeEach(^{
+            sut = [[CRVSettingsMessenger alloc] init];
+        });
+        
+        afterEach(^{
+            sut = nil;
+        });
+        
+        it(@"should not be nil", ^{
+            expect(sut).toNot.beNil();
+        });
+        
+        it(@"should post done notifiction", ^{
+            expect(^{
+                [sut postDoneMessage];
+            }).will.notify(@"CRVImageEditViewControllerWillFinishEditingWithImageAssetNotification");
+        });
+        
+        it(@"should post cancel notifiction", ^{
+            expect(^{
+                [sut postCancelMessage];
+            }).will.notify(@"CRVImageEditViewControllerWillCancelEditingNotification");
+        });
+        
+        it(@"should post ratio notifiction", ^{
+            expect(^{
+                [sut postShowRatioSheetMessage];
+            }).will.notify(@"CRVImageEditViewControllerWillShowRatioAlertControllerNotification");
+        });
+        
+        it(@"should post reset notifiction", ^{
+            expect(^{
+                [sut postResetTransformationMessage];
+            }).will.notify(@"CRVTransformViewControllerWillResetTransformationNotification");
+        });
+    });
+});
+
+SpecEnd
